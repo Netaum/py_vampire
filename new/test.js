f548371f6d742e235b02be9fea639239633be597
Antonio Felipe Cechi Neto <pronetao@gmail.com>
2018-10-15 23:10:24 -0300

    New fonts

--- new/test.js
+++ new/test.js
@@ -2,37 +2,37 @@ $(function(){
     $(".wing").click(function(){
         var getSelection = window.getSelection();
         var selection = getSelection.focusOffset;
-        var text = $("span", this).text();
+        var text = $(this).text();
         var newText = "";
         if(selection === 0){
             var f = text.substring(0,1);
-            if(f==='m'){
-                newText = "qmmmm";
+            if(f==='d'){
+                newText = "edddd";
             }
             else{
-                newText = "mmmmm";
+                newText = "ddddd";
             }
         }
         else{
             for(i = 0; i < 5; i++){
                 if(i < selection){
-                    newText += "q";
+                    newText += "e";
                 }
                 else if(i === selection){
                     var f = text.substring(i,i+1);
-                    if(f === 'm'){
-                        newText += "q";
+                    if(f === 'd'){
+                        newText += "e";
                     }
                     else{
-                        newText += "m"
+                        newText += "d"
                     }
                 }
                 else{
-                    newText += "m"
+                    newText += "d"
                 }
             }
         }
-        $("span", this).text(newText);
+        $(this).text(newText);
     });
 
 });
