0796367119d09f90fb0776d5b086a85e69b1d2eb
Antonio Felipe Cechi Neto <antoniofc@ciandt.com>
2018-10-17 16:27:23 -0300

    More more more

--- new/test.js
+++ new/test.js
@@ -4,34 +4,25 @@ $(function(){
         var selection = getSelection.focusOffset;
         var text = $(this).text();
         var newText = "";
-        if(selection === 0){
-            var f = text.substring(0,1);
-            if(f==='d'){
-                newText = "edddd";
+
+        for(i = 0; i < 5; i++){
+            if(i < selection){
+                newText += "e";
             }
-            else{
-                newText = "ddddd";
-            }
-        }
-        else{
-            for(i = 0; i < 5; i++){
-                if(i < selection){
+            else if(i === selection){
+                var f = text.substring(i,i+1);
+                if(f === 'd'){
                     newText += "e";
-                }
-                else if(i === selection){
-                    var f = text.substring(i,i+1);
-                    if(f === 'd'){
-                        newText += "e";
-                    }
-                    else{
-                        newText += "d"
-                    }
                 }
                 else{
                     newText += "d"
                 }
             }
+            else{
+                newText += "d"
+            }
         }
+
         $(this).text(newText);
     });
 
